0e65c7bef0047097f6e7adc2bbe3a625c1f3e5ac
김동규 <eastar385@naver.com>
2024-11-22 11:33:48 +0900

    example10

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Home from "./page";
-import React from "react";
 
 describe("Home", () => {
   it("renders the page", async () => {
